8d9e7c4247b521ab6c5c0c7332f6d9336115b73e
sinkrons <balasa.edward@gmail.com>
2023-10-25 18:16:25 +0300

    first commit

---
+++ src/main/java/com/example/devopsproject/DevopsProjectApplication.java
@@ -0,0 +1,19 @@
+package com.example.devopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsProjectApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
